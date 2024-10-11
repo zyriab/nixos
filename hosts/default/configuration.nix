550450e6f8400143027853856a5f633d80f2511b
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-12 00:35:07 +0200

    chore: update paths

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -27,15 +27,15 @@ in
 
     ### Aliases
     environment.shellAliases = {
-        nrb = "sudo nixos-rebuild switch --flake ~/nixos/#default";
-        ned = "nvim ~/nixos/hosts/default/configuration.nix";
-        hed = "nvim ~/nixos/hosts/default/home.nix";
+        nrb = "sudo nixos-rebuild switch --flake ~/.config/nixos/#default";
+        ned = "nvim ~/.config/nixos/hosts/default/configuration.nix";
+        hed = "nvim ~/.config/nixos/hosts/default/home.nix";
         ll = "ls -l";
         lg = "lazygit";
         nst = "npm start";
         got = "go test ./...";
         qc = "qmk compile -e CONVERT_TO=promicro_rp2040 -km colemak-v1";
-        brs = "browsh --startup-url search.nixos.org";
+        brs = "browsh --startup-url https://search.nixos.org";
     };
 
     ### Env vars
