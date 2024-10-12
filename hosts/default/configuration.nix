9aa5353edba298974698a0d8bd59176c6176755c
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-12 02:02:19 +0200

    chore: move aliases to own file

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -17,26 +17,14 @@ in
 {
     imports = [
         ./hardware-configuration.nix
-        ../../modules/nixos/main-user.nix
+        ../../modules/system/main-user.nix
+        ../../modules/system/aliases.nix
     ];
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     # Preventing the laptop from sleeping on lid close
     services.logind.lidSwitchExternalPower = "ignore";
-
-    ### Aliases
-    environment.shellAliases = {
-        nrb = "sudo nixos-rebuild switch --flake ~/.config/nixos/#default";
-        ned = "nvim ~/.config/nixos/hosts/default/configuration.nix";
-        hed = "nvim ~/.config/nixos/hosts/default/home.nix";
-        ll = "ls -l";
-        lg = "lazygit";
-        nst = "npm start";
-        got = "go test ./...";
-        qc = "qmk compile -e CONVERT_TO=promicro_rp2040 -km colemak-v1";
-        brs = "browsh --startup-url https://search.nixos.org";
-    };
 
     ### Env vars
     environment.variables = {
