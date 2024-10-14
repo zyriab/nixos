ae008c056f086abf6d9259e3398ddfc813efc12b
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-14 22:55:32 +0200

    feat: use nvim as manpager

--- modules/system/env-vars.nix
+++ modules/system/env-vars.nix
@@ -5,6 +5,10 @@
     environment.variables = {
       EDITOR = "nvim";
       TERM = "screen-256color";
+
+      # Using Neovim as manpager
+      MANPAGER = "nvim +Man!";
+      MANWIDTH = 999;
     };
   };
 }
