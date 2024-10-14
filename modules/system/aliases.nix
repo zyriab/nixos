c88057a9a6d9c7e8596692dbb2d37704b369eea9
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-14 22:55:23 +0200

    chore: add alias

--- modules/system/aliases.nix
+++ modules/system/aliases.nix
@@ -6,12 +6,13 @@
       nrb = "sudo nixos-rebuild switch --flake ~/.config/nixos/#default";
       ned = "nvim ~/.config/nixos/hosts/default/configuration.nix";
       hed = "nvim ~/.config/nixos/hosts/default/home.nix";
+      ncg = "nvim ~/.config/nixos/";
       ll = "ls -l";
       lg = "lazygit";
       nst = "npm start";
       got = "go test ./...";
       qc = "qmk compile -e CONVERT_TO=promicro_rp2040 -km colemak-v1";
-      brs = "browsh --startup-url https://search.nixos.org";
+      brs = "browsh";
     };
   };
 }
