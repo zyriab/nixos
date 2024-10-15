997276b2c6cb30fddb7eb37e9c302510c8395389
Arthur Wallendorff <howdy@wallenart.dev>
2024-10-15 14:40:59 +0200

    chore: alphabetically sort aliases

--- modules/system/aliases.nix
+++ modules/system/aliases.nix
@@ -3,16 +3,16 @@
 {
   config = {
     environment.shellAliases = {
-      nrb = "sudo nixos-rebuild switch --flake ~/.config/nixos/#default";
-      ned = "nvim ~/.config/nixos/hosts/default/configuration.nix";
-      hed = "nvim ~/.config/nixos/hosts/default/home.nix";
-      ncg = "nvim ~/.config/nixos/";
-      ll = "ls -l";
-      lg = "lazygit";
-      nst = "npm start";
-      got = "go test ./...";
-      qc = "qmk compile -e CONVERT_TO=promicro_rp2040 -km colemak-v1";
       brs = "browsh";
+      got = "go test ./...";
+      hed = "nvim ~/.config/nixos/hosts/default/home.nix";
+      lg = "lazygit";
+      ll = "ls -l";
+      ncg = "nvim ~/.config/nixos/";
+      ned = "nvim ~/.config/nixos/hosts/default/configuration.nix";
+      nrb = "sudo nixos-rebuild switch --flake ~/.config/nixos/#default";
+      nst = "npm start";
+      qc = "qmk compile -e CONVERT_TO=promicro_rp2040 -km colemak-v1";
       tmx = "tmux new -A -s lab";
     };
   };
